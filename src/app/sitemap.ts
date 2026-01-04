75ffd4bc45e7fa92a83dd5b6d93fa7b3752cb08e
jobelGolde12 <jobelgolde43@gmail.com>
2026-01-04 12:46:46 +0800

    Modifying sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,4 +1,7 @@
-export default function sitemap() {
+// app/sitemap.ts
+import { MetadataRoute } from 'next';
+
+export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
       url: 'https://whatshouldido-five.vercel.app/',
